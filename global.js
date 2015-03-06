ddebbea64477a07636df08f3cfa49d228662e206
Victor Larsson <victor@dictodesigns.com>
2015-03-06 16:01:04 +0100

    Fixed unintentional leave. Forces a dialog.

--- global.js
+++ global.js
@@ -13,3 +13,20 @@ function reply(string) {
 function log(string) {
 	$("#console").append('<span class="log">' + string + '<span><br><br>');
 }
+
+// http://stackoverflow.com/questions/1119289/how-to-show-the-are-you-sure-you-want-to-navigate-away-from-this-page-when-ch
+window.onbeforeunload = function (e) {
+    // If we haven't been passed the event get the window.event
+    e = e || window.event;
+
+    var message = 'Any text will block the navigation and display a prompt';
+
+    // For IE6-8 and Firefox prior to version 4
+    if (e) 
+    {
+    	e.returnValue = message;
+    }
+
+    // For Chrome, Safari, IE8+ and Opera 12+
+    return message;
+};
